c6a52054222a04514f0c04e71255c2d73a303f48
hyunukJung <moredream7@gmail.com>
2012-04-17 14:55:09 +0900

    First commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+BlogMongo::Application.config.secret_token = 'ec04a53b189d5b33aabd1460b458cef56fdf2d3ccd2642380d926db4acaf19d5a7244d89861b32f08cc3282e2e560fcce1388732cff562c4e18c4df45cfddb1e'
